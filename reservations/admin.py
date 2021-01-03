dc3ad6ea83004ae0245cdf183a227c45c6725b08
Nji Daniel <njid18753@gmail.com>
2021-01-04 00:49:52 +0100

    Add files via upload

---
+++ reservations/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from . import models
+
+
+@admin.register(models.Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+
+    """ Reservation Admin Definition """
+
+    list_display = (
+        "room",
+        "status",
+        "check_in",
+        "check_out",
+        "guest",
+        "in_progress",
+        "is_finished",
+    )
+
+    list_filter = ("status",)
+
