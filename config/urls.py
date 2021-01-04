af370e28506d5a1a2655c470dc8df5531ca7a633
Nji Daniel <njid18753@gmail.com>
2021-01-04 05:18:30 +0100

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,6 @@
 """config URL Configuration
 
+
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
